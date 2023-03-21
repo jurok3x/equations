e422d5ab25c8501d9de1e8efe1f40def23b69a65
ykotsiuba <jurok3x@gmail.com>
2023-03-21 14:19:37 +0200

    First commit

---
+++ src/test/java/com/yukotsiuba/equation/EquationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukotsiuba.equation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
