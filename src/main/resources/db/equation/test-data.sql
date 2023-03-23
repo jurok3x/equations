eec8efa6fc28ae98ab8ea5318b49a1c5f4f09fb6
ykotsiuba <jurok3x@gmail.com>
2023-03-23 12:27:14 +0200

    Added test database configuration with H2 database.

---
+++ src/main/resources/db/equation/test-data.sql
@@ -0,0 +1,4 @@
+MERGE INTO equations(id, equation) VALUES (1, '-8.7*4/x=4.3');
+MERGE INTO equations(id, equation) VALUES (2, '122*x+5--3=174');
+MERGE INTO equations(id, equation) VALUES (3, '0.2*-x+5=17');
+MERGE INTO equations(id, equation) VALUES (4, '(2-3)/x=5-3-1+x');
