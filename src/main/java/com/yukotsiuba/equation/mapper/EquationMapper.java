542e9c6eabe17669a5ccb1c783389a0933a0a632
ykotsiuba <jurok3x@gmail.com>
2023-03-25 17:55:22 +0200

    Fixed dto and equation mapper.

--- src/main/java/com/yukotsiuba/equation/mapper/EquationMapper.java
+++ src/main/java/com/yukotsiuba/equation/mapper/EquationMapper.java
@@ -1,21 +1,39 @@
 package com.yukotsiuba.equation.mapper;
 
 import com.yukotsiuba.equation.dto.EquationDto;
+import com.yukotsiuba.equation.dto.RootDto;
 import com.yukotsiuba.equation.entity.Equation;
+import com.yukotsiuba.equation.entity.Root;
+
+import java.util.List;
 
 public class EquationMapper {
 
     public static EquationDto toDto(Equation equation) {
-        return EquationDto.builder()
+        EquationDto equationDto = EquationDto.builder()
                 .id(equation.getId())
                 .eqString(equation.getEqString())
                 .build();
+        if(equation.getRoots() != null) {
+            List<RootDto> rootDtos = equation.getRoots().stream()
+                    .map(RootMapper::toDto)
+                    .toList();
+            equationDto.setRoots(rootDtos);
+        }
+        return equationDto;
     }
 
     public static Equation toEntity(EquationDto equationDto) {
-        return Equation.builder()
+        Equation equation = Equation.builder()
                 .id(equationDto.getId())
                 .eqString(equationDto.getEqString())
                 .build();
+        if(equationDto.getRoots() != null) {
+            List<Root> roots = equationDto.getRoots().stream()
+                    .map(RootMapper::toEntity)
+                    .toList();
+            equation.setRoots(roots);
+        }
+        return equation;
     }
 }
